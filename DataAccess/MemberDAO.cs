9c5739368f7ae1179b324b106168454b021a42bf
quocbahuynh <jbrteamwebsite@gmail.com>
2022-07-04 17:32:01 +0700

    get by city and country

--- DataAccess/MemberDAO.cs
+++ DataAccess/MemberDAO.cs
@@ -130,5 +130,43 @@ namespace DataAccess
             var sortedList = members.OrderByDescending(x => x.MemberName).ToList();
             return sortedList;
         }
+
+        public IEnumerable<MemberObject> GetMemberByCityAndName(string city, string country)
+        {
+            IDataReader? dataReader = null;
+            string SQLSelect = "Select MemberID, MemberName, Email, Password, City, Country " +
+                " from Members" +
+                " where City = @City and Country =@Country";
+            var members = new List<MemberObject>();
+            try
+            {
+                var param = new List<SqlParameter>();
+                param.Add(dataProvider.CreateParameter("@City", 50, city, DbType.String));
+                param.Add(dataProvider.CreateParameter("@Country", 50, country, DbType.String));
+                dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection, param.ToArray());
+                while (dataReader.Read())
+                {
+                    members.Add(new MemberObject
+                    {
+                        MemberID = dataReader.GetString(0),
+                        MemberName = dataReader.GetString(1),
+                        Email = dataReader.GetString(2),
+                        Password = dataReader.GetString(3),
+                        City = dataReader.GetString(4),
+                        Country = dataReader.GetString(5)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                dataReader.Close();
+                CloseConnection();
+            }
+            return members;
+        }
     }
 }
--- DataAccess/Repository/MemberRepository.cs
+++ DataAccess/Repository/MemberRepository.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObject;
 
 namespace DataAccess.Repository
 {
     internal class MemberRepository
     {
-         public IEnumerable<MemberObject> SortDesByName() => MemberDAO.Instance.SortDesByName();
+        public IEnumerable<MemberObject> SortDesByName() => MemberDAO.Instance.SortDesByName();
+        public IEnumerable<MemberObject> GetMemberByCityAndCountry(string city, string country) => MemberDAO.Instance.GetMemberByCityAndName(city, country);
     }
 }
